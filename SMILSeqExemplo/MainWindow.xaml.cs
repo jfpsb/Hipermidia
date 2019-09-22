908c81543170b57cd2e6b4d1673056f25ad6c9c7
Felipe <jfpsb_felipe2@hotmail.com>
2019-09-22 12:33:10 -0300

    projeto

---
+++ SMILSeqExemplo/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using System.Xml;
+
+namespace SMILSeqExemplo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private XmlDocument xmlDocument;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            xmlDocument = new XmlDocument();
+
+            xmlDocument.Load(@"C:\Users\jfpsb\Downloads\exemplo01\slideshow.xml");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(bah, DispatcherPriority.ContextIdle, null);
+        }
+
+        private void bah()
+        {
+            xmlDocument = new XmlDocument();
+
+            xmlDocument.Load(@"C:\Users\jfpsb\Downloads\exemplo01\slideshow.xml");
+
+            if (xmlDocument.DocumentElement.Name == "smil")
+            {
+                XmlNodeList imgList = xmlDocument.GetElementsByTagName("img");
+
+                foreach (XmlNode imgNode in imgList)
+                {
+                    if (imgNode.Name == "img")
+                    {
+                        imageHolder.Source = new BitmapImage(new Uri(@"C:\Users\jfpsb\Downloads\exemplo01\" + imgNode.Attributes["src"].Value));
+                        Thread.Sleep(500);
+                    }
+                }
+            }
+        }
+    }
+}
