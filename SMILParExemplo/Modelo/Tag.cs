2129be74e9074befb4184b31592f029ac5a79777
Felipe <jfpsb_felipe2@hotmail.com>
2019-10-23 17:50:52 -0300

    Tentando com MVC

---
+++ SMILParExemplo/Modelo/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Windows.Controls;
+
+namespace SMILParExemplo.Modelo
+{
+    public abstract class Tag
+    {
+        public Grid Grid { get; set; }
+        public Thread Thread { get; set; }
+        public int Duration { get; set; }
+    }
+}
