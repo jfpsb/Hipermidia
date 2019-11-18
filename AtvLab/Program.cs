9d6a7a7c7eb1f948c82f79d59f4c5e21b1ba5739
Felipe <jfpsb_felipe2@hotmail.com>
2019-11-18 17:35:27 -0300

    Atividade tokens

---
+++ AtvLab/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace AtvLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textoCompleto = File.ReadAllText(@"C:\Users\jfpsb\Downloads\testCollection.dat\testCollection.dat", Encoding.UTF8).Replace("\n", "");
+            string textoCollection = textoCompleto.Split(new string[] { "<collection>", "</collection>" }, StringSplitOptions.RemoveEmptyEntries)[0];
+            string[] pages = textoCollection.Split(new string[] { "<page>", "</page>" }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, List<Token>> tokens = new Dictionary<string, List<Token>>();
+
+            foreach(string page in pages)
+            {
+                string id = page.Split(new string[] { "<id>", "</id>" }, StringSplitOptions.RemoveEmptyEntries)[0];
+                string title = page.Split(new string[] { "<title>", "</title>" }, StringSplitOptions.RemoveEmptyEntries)[1];
+                string text = page.Split(new string[] { "<text>", "</text>" }, StringSplitOptions.RemoveEmptyEntries)[1];
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AtvLab/Token.cs
@@ -0,0 +1,9 @@
+﻿namespace AtvLab
+{
+    public class Token
+    {
+        public int Id { get; set; }
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+    }
+}
